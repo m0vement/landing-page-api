84be1877ca653ef52e06b51ed4a9b480c92d3ebb
Inderpal Singh <inderpal@pfeffermind-games.de>
2015-11-15 18:42:32 +0100

    typo

--- start.js
+++ start.js
@@ -2,5 +2,5 @@ require('babel/register');
 var Mongoose = require ('mongoose');
 var createServer = require('./src/server');
 
-Mongoose.connect(~~process.env.MONGOLAB_URI || 'mongodb://localhost/signups');
+Mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/signups');
 createServer(~~process.env.PORT || 3000);
